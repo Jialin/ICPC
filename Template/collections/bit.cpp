5a948728681de022dc9ee3df61cb241b4c37bd9b
Jialin Ouyang <Jialin.Ouyang@gmail.com>
2019-03-05 17:39:47 -0800

    binary indexed tree

---
+++ Template/collections/bit.cpp
@@ -0,0 +1,56 @@
+/**
+ * Code attached for convenience.
+ */
+namespace mu {
+
+// Checks whether the number is in the form of 2^x
+inline bool isPow2(int n) {
+  return n && !(n & (n - 1));
+}
+
+// Computes the smallest 2^x which is not less than n.
+inline int nextPow2(int n) {
+  return isPow2(n) ? n : 1 << (32 - __builtin_clz(static_cast<unsigned int>(n)));
+}
+} // namespace mu
+
+namespace cs { namespace bit {
+
+template <typename T>
+class BinaryIndexedTree {
+  int n;
+  vector<T> vs;
+
+  inline static int transform(int i) {
+    return (i & (i + 1)) - 1;
+  }
+
+public:
+  // Initializes
+  inline void init(int n_, bool fixed = true) {
+    this->n = n_;
+    vs.reserve(fixed ? n : mu::nextPow2(n));
+    vs.resize(n);
+    fill(ALL(vs), 0);
+  }
+
+  inline void update(int i, const T &delta) {
+    for (; i < n; i |= i + 1) {
+      vs[i] += delta;
+    }
+  }
+
+  inline T calc(int i) {
+    if (i < 0) return T();
+    T res = vs[i];
+    for (i = transform(i); i >= 0; i = transform(i)) {
+      res += vs[i];
+    }
+    return res;
+  }
+
+  inline T calcRange(int i, int j) {
+    return calc(j) - calc(i - 1);
+  }
+};
+}} // namespace cs::bit
--- Template/mathutil.cpp
+++ Template/mathutil.cpp
@@ -1,16 +1,28 @@
-namespace mathutil {
+namespace mu {
 
-  template<typename T>
-  T gcd(T a, T b) {
-    if (a < 0) a = -a;
-    if (b < 0) b = -b;
-    while (a > 0 && b > 0) {
-      if (a > b) {
-        a %= b;
-      } else {
-        b %= a;
-      }
+// Computes greatest common divisor
+template<typename T>
+inline T gcd(T a, T b) {
+  if (a < 0) a = -a;
+  if (b < 0) b = -b;
+  while (a > 0 && b > 0) {
+    if (a > b) {
+      a %= b;
+    } else {
+      b %= a;
     }
-    return a + b;
   }
-} // mathutil
+  return a + b;
+}
+
+// Checks whether the number is in the form of 2^x
+inline bool isPow2(int n) {
+  return n && !(n & (n - 1));
+}
+
+// Computes the smallest 2^x which is not less than n.
+inline int nextPow2(int n) {
+  if (n <= 1) return 1;
+  return isPow2(n) ? n : 1 << (32 - __builtin_clz(static_cast<unsigned int>(n)));
+}
+} // namespace mu
