19bff8e8cfb459f09df2d2ec0ae80a8fd50483ed
Jialin Ouyang <Jialin.Ouyang@gmail.com>
2019-03-06 19:05:03 -0800

    Create lis.cpp

---
+++ Template/collections/lis.cpp
@@ -0,0 +1,26 @@
+namespace cs { namespace lis {
+
+template<typename T>
+class LongestIncreasingSubsequence {
+  vector<T> lis;
+
+public:
+  // Length of lis
+  int length;
+
+  // Initializes
+  inline void init(int n) {
+    lis.resize(n);
+    length = 0;
+  }
+
+  // Try to add v to lis
+  inline void add(const T& v) {
+    T absv = abs(v);
+    const auto& lisBegin = lis.begin();
+    long idx = lower_bound(lisBegin, lisBegin + length, absv) - lisBegin;
+    lis[idx] = absv;
+    if (idx == length) ++length;
+  }
+};
+}} // namespace cs::lis
