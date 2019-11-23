0d8ef056df6c681b258aefb4b25103575d6b33ab
Jialin Ouyang <Jialin.Ouyang@gmail.com>
2019-11-23 14:18:51 -0800

    [Template][Convex hull tricks] Move all into the class

--- Template/collections/convex_hull_tricks/convex_hull_tricks.cpp
+++ Template/collections/convex_hull_tricks/convex_hull_tricks.cpp
@@ -1,103 +1,10 @@
 namespace collections {
 
-namespace {
-
-using MaxUInt = uint64_t;
-const MaxUInt kMax = numeric_limits<MaxUInt>::max();
-const int kHalfL = numeric_limits<MaxUInt>::digits >> 1;
-const MaxUInt kHalfMask = kMax >> kHalfL;
-
-inline bool isProductOverflow(MaxUInt a, MaxUInt b) {
-  if (!a || !b) {
-    return false;
-  }
-  return a > kMax / b;
-}
-
-inline void product(MaxUInt a, MaxUInt b, MaxUInt &high, MaxUInt &low) {
-  MaxUInt aHigh = a >> kHalfL, aLow = a & kHalfMask;
-  MaxUInt bHigh = b >> kHalfL, bLow = b & kHalfMask;
-  high = aHigh * bHigh;
-  low = aLow * bLow;
-  MaxUInt ab = aHigh * bLow, ba = bHigh * aLow;
-  high += (ab >> kHalfL) + (ba >> kHalfL);
-  MaxUInt lowHigh = (low >> kHalfL) + (ab & kHalfMask) + (ba & kHalfMask);
-  high += lowHigh >> kHalfL;
-  low = ((lowHigh & kHalfMask) << kHalfL) + (low & kHalfMask);
-}
-
-inline int sgn(MaxUInt a, MaxUInt b) { return a < b ? -1 : a == b ? 0 : 1; }
-
-/** sgn(a*b-c*d) */
-inline int sgnUnsignedProduct(MaxUInt a, MaxUInt b, MaxUInt c, MaxUInt d) {
-  bool overflowAB = isProductOverflow(a, b);
-  bool overflowCD = isProductOverflow(c, d);
-  if (overflowAB) {
-    if (overflowCD) {
-      MaxUInt high1, low1, high2, low2;
-      product(a, b, high1, low1);
-      product(c, d, high2, low2);
-      int sgnHigh = sgn(high1, high2);
-      return sgnHigh ? sgnHigh : sgn(low1, low2);
-    } else {
-      return 1;
-    }
-  } else {
-    if (overflowCD) {
-      return -1;
-    } else {
-      return sgn(a * b, c * d);
-    }
-  }
-}
-
-/** sgn(a/b-c/d) */
-template <typename T> inline int sgnFraction(T a, T b, T c, T d) {
-  if (b < 0) {
-    a = -a;
-    b = -b;
-  }
-  if (d < 0) {
-    c = -c;
-    d = -d;
-  }
-  if (a < 0) {
-    if (c < 0) {
-      return -sgnUnsignedProduct(-a, d, -c, b);
-    } else {
-      return -1;
-    }
-  } else {
-    if (c < 0) {
-      return 1;
-    } else {
-      return sgnUnsignedProduct(a, d, c, b);
-    }
-  }
-}
-
-template <typename T> class Line {
-public:
-  T a, b;
-  mutable T num, den;
-
-  inline Line(T a_, T b_) : a(a_), b(b_), num(0), den(1) {}
-
-  inline bool operator<(const Line<T> &o) const { return a < o.a; }
-
-  inline bool operator<(T x) const {
-    return sgnFraction<T>(num, den, x, 1) < 0;
-  }
-};
-
-} // namespace
-
-template <typename T>
-class ConvexHullTricks : public multiset<Line<T>, less<>> {
+template <typename T, typename CMP_UINT_T = uint64_t> class ConvexHullTricks {
 public:
   inline void initQueryMax(bool queryMax_) {
     queryMax = queryMax_;
-    this->clear();
+    lines_.clear();
   }
 
   inline void add(T a, T b) {
@@ -105,42 +12,69 @@ public:
       a = -a;
       b = -b;
     }
-    auto y = this->emplace(a, b);
+    auto y = lines_.emplace(a, b);
     // Remove following lines
     auto z = next(y);
     update(y, z);
     while (!inOrder(y, z)) {
-      z = this->erase(z);
+      z = lines_.erase(z);
       update(y, z);
     }
     // Remove newly added line
-    if (y == this->begin()) {
+    if (y == lines_.begin()) {
       return;
     }
     auto x = prev(y);
     update(x, y);
     if (!inOrder(x, y)) {
-      y = this->erase(y);
+      y = lines_.erase(y);
       update(x, y);
     }
     // Remove preceeding lines
-    for (y = x; x != this->begin() && !inOrder(--x, y); y = x) {
-      update(x, this->erase(y));
+    for (y = x; x != lines_.begin() && !inOrder(--x, y); y = x) {
+      update(x, lines_.erase(y));
     }
   }
 
   inline T query(T x) const {
-    assert(!this->empty());
-    auto l = this->lower_bound(x);
+    assert(!lines_.empty());
+    auto l = lines_.lower_bound(x);
     T res = l->a * x + l->b;
     return queryMax ? res : -res;
   }
 
+  inline int size() { return static_cast<int>(lines_.size()); }
+
+  inline void lines(const function<void(T, T)> &lineProcessor) {
+    for (const Line &line : lines_) {
+      if (queryMax) {
+        lineProcessor(line.a, line.b);
+      } else {
+        lineProcessor(-line.a, -line.b);
+      }
+    }
+  }
+
 private:
-  using Iterator = typename multiset<Line<T>, less<>>::iterator;
+  class Line {
+  public:
+    T a, b;
+    mutable T num, den;
+
+    inline Line(T a_, T b_) : a(a_), b(b_), num(0), den(1) {}
+
+    inline bool operator<(const Line &o) const { return a < o.a; }
+
+    inline bool operator<(T x) const { return sgnFraction(num, den, x, 1) < 0; }
+  };
+
+  using Iterator = typename multiset<Line, less<>>::iterator;
+
+  bool queryMax;
+  multiset<Line, less<>> lines_;
 
   inline void update(const Iterator &x, const Iterator &y) {
-    if (y == this->end()) {
+    if (y == lines_.end()) {
       x->num = 1;
       x->den = 0;
     } else if (x->a == y->a) {
@@ -153,11 +87,85 @@ private:
   }
 
   inline bool inOrder(const Iterator &x, const Iterator &y) const {
-    return y == this->end() ||
-           sgnFraction<T>(x->num, x->den, y->num, y->den) < 0;
+    return y == lines_.end() || sgnFraction(x->num, x->den, y->num, y->den) < 0;
   }
 
-  bool queryMax;
+  static const CMP_UINT_T kMax = numeric_limits<CMP_UINT_T>::max();
+  static const int kHalfL = numeric_limits<CMP_UINT_T>::digits >> 1;
+  static const CMP_UINT_T kHalfMask = kMax >> kHalfL;
+
+  inline static bool isProductOverflow(CMP_UINT_T a, CMP_UINT_T b) {
+    if (!a || !b) {
+      return false;
+    }
+    return a > kMax / b;
+  }
+
+  inline static void product(CMP_UINT_T a, CMP_UINT_T b, CMP_UINT_T &high,
+                             CMP_UINT_T &low) {
+    CMP_UINT_T aHigh = a >> kHalfL, aLow = a & kHalfMask;
+    CMP_UINT_T bHigh = b >> kHalfL, bLow = b & kHalfMask;
+    high = aHigh * bHigh;
+    low = aLow * bLow;
+    CMP_UINT_T ab = aHigh * bLow, ba = bHigh * aLow;
+    high += (ab >> kHalfL) + (ba >> kHalfL);
+    CMP_UINT_T lowHigh = (low >> kHalfL) + (ab & kHalfMask) + (ba & kHalfMask);
+    high += lowHigh >> kHalfL;
+    low = ((lowHigh & kHalfMask) << kHalfL) + (low & kHalfMask);
+  }
+
+  inline static int sgn(CMP_UINT_T a, CMP_UINT_T b) {
+    return a < b ? -1 : a == b ? 0 : 1;
+  }
+
+  /** sgn(a*b-c*d) */
+  inline static int sgnUnsignedProduct(CMP_UINT_T a, CMP_UINT_T b, CMP_UINT_T c,
+                                       CMP_UINT_T d) {
+    bool overflowAB = isProductOverflow(a, b);
+    bool overflowCD = isProductOverflow(c, d);
+    if (overflowAB) {
+      if (overflowCD) {
+        CMP_UINT_T high1, low1, high2, low2;
+        product(a, b, high1, low1);
+        product(c, d, high2, low2);
+        int sgnHigh = sgn(high1, high2);
+        return sgnHigh ? sgnHigh : sgn(low1, low2);
+      } else {
+        return 1;
+      }
+    } else {
+      if (overflowCD) {
+        return -1;
+      } else {
+        return sgn(a * b, c * d);
+      }
+    }
+  }
+
+  /** sgn(a/b-c/d) */
+  inline static int sgnFraction(T a, T b, T c, T d) {
+    if (b < 0) {
+      a = -a;
+      b = -b;
+    }
+    if (d < 0) {
+      c = -c;
+      d = -d;
+    }
+    if (a < 0) {
+      if (c < 0) {
+        return sgnUnsignedProduct(-c, b, -a, d);
+      } else {
+        return -1;
+      }
+    } else {
+      if (c < 0) {
+        return 1;
+      } else {
+        return sgnUnsignedProduct(a, d, c, b);
+      }
+    }
+  }
 };
 
 } // namespace collections
