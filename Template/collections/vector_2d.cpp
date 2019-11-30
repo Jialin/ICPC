4fc63d2b834aecb5463940aa40360ea3192c747e
Jialin Ouyang <Jialin.Ouyang@gmail.com>
2019-11-30 01:29:34 -0800

    [Template][collections] vector 2d

--- Template/collections/vector_2d.cpp
+++ Template/collections/vector_2d.cpp
@@ -1,30 +1,26 @@
-namespace cs {
+namespace collections {
 
-template<typename V>
-class Vector2D {
+template <typename T> class Vector2D {
 public:
   inline void init(int n, int m = -1) {
-    n_ = n;
-    lastOut_.resize(n); fill(lastOut_.begin(), lastOut_.end(), -1);
-    nxtOut_.clear(); if (m >= 0) nxtOut_.reserve(m);
-    vs_.clear(); if (m >= 0) vs_.reserve(m);
+    lastOut.assign(n, -1);
+    nxtOut.clear();
+    values.clear();
+    if (m > 0) {
+      nxtOut.reserve(m);
+      values.reserve(m);
+    }
   }
 
-  inline void add(int x, const V& v) {
-    int edgeIdx = SIZE(nxtOut_);
-    nxtOut_.push_back(lastOut_[x]);
-    lastOut_[x] = edgeIdx;
-    vs_.push_back(v);
+  inline void add(int x, T v) {
+    int edgeIdx = static_cast<int>(nxtOut.size());
+    nxtOut.push_back(lastOut[x]);
+    lastOut[x] = edgeIdx;
+    values.push_back(v);
   }
 
-  inline int n() const { return n_; }
-  inline int lastOut(int x) const { return lastOut_[x]; }
-  inline int nxtOut(int x) const { return nxtOut_[x]; }
-  inline int value(int x) const { return vs_[x]; }
+  vector<int> lastOut, nxtOut;
+  vector<T> values;
+};
 
-private:
-  int n_;
-  vector<int> lastOut_, nxtOut_;
-  vector<V> vs_;
-}; // class Vector2D
-} // namespace cs
+} // namespace collections
