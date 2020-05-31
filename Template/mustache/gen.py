0c8839a68b41b320ceca13f6670605fbb67b1c75
Jialin Ouyang <Jialin.Ouyang@gmail.com>
2020-05-31 14:33:35 -0700

    [mustache] gen

--- Template/mustache/gen.py
+++ Template/mustache/gen.py
@@ -1,32 +1,38 @@
 import os
 import sys
 
-from collections_.convex_hull_tricks.convex_hull_tricks import \
-    JOBS as collectionsConvexHullTricksJobs
-from collections_.convex_hull_tricks.convex_hull_tricks_mono_add import \
-    JOBS as collectionsConvexHullTricksMonoAddJobs
-from collections_.convex_hull_tricks.convex_hull_tricks_mono_add_query import \
-    JOBS as collectionsConvexHullTricksMonoAddQueryJobs
-from collections_.convex_hull_tricks.li_chao_tree_known_queries import \
-    JOBS as collectionsConvexHullTricksLiChaoTreeKnownQueries
+from collections_.convex_hull_tricks.convex_hull_tricks import (
+    JOBS as collectionsConvexHullTricksJobs,
+)
+from collections_.convex_hull_tricks.convex_hull_tricks_mono_add import (
+    JOBS as collectionsConvexHullTricksMonoAddJobs,
+)
+from collections_.convex_hull_tricks.convex_hull_tricks_mono_add_query import (
+    JOBS as collectionsConvexHullTricksMonoAddQueryJobs,
+)
+from collections_.convex_hull_tricks.li_chao_tree_known_queries import (
+    JOBS as collectionsConvexHullTricksLiChaoTreeKnownQueries,
+)
 from math_.floor_div import JOBS as mathFloorDivJobs
+from math_.mod_utils import JOBS as mathModUtilJobs
 from math_.sgn import JOBS as mathSgnJobs
 from math_.sgn_fraction import JOBS as mathSgnFractionJobs
 from math_.sgn_product_unsigned import JOBS as mathSgnProductUnsignedJobs
 
-ALL_JOBS = [collectionsConvexHullTricksJobs,
-            collectionsConvexHullTricksMonoAddQueryJobs,
-            collectionsConvexHullTricksMonoAddJobs,
-            collectionsConvexHullTricksLiChaoTreeKnownQueries,
-            mathFloorDivJobs,
-            mathSgnFractionJobs,
-            mathSgnJobs,
-            mathSgnProductUnsignedJobs]
+ALL_JOBS = [
+    collectionsConvexHullTricksJobs,
+    collectionsConvexHullTricksMonoAddQueryJobs,
+    collectionsConvexHullTricksMonoAddJobs,
+    collectionsConvexHullTricksLiChaoTreeKnownQueries,
+    mathFloorDivJobs,
+    mathModUtilJobs,
+    mathSgnJobs,
+    mathSgnFractionJobs,
+    mathSgnProductUnsignedJobs,
+]
 
 for jobs in ALL_JOBS:
-  # print(jobs)
-  for mustache, contexts in jobs:
-    # print('\t', mustache, contexts)
-    for context in contexts:
-      # print('\t\t', context)
-      mustache.genFile(context)
+    for mustache, contexts in jobs:
+        for context in contexts:
+            # print("Handling mustache:{} context:{}".format(mustache, context))
+            mustache.genFile(context)
