7377c148ab9e29093b39a5ba210d483a67f870d5
Jialin Ouyang <Jialin.Ouyang@gmail.com>
2020-05-30 17:18:42 -0700

    [Template][math][mod_operators] fix_inline.h

--- Template/math/mod_operators/fix_inline.h
+++ Template/math/mod_operators/fix_inline.h
@@ -7,11 +7,11 @@
 namespace math {
 
 template <typename V = int32_t> inline void fixModInline(V &v, const V &mod) {
-  slightFixModInline(v, mod);
+  slightFixModInline<V>(v, mod);
   if (v < 0 || mod <= v) {
     assert(mod);
     v %= mod;
-    slightFixModInline(v, mod);
+    slightFixModInline<V>(v, mod);
   }
 }
 
