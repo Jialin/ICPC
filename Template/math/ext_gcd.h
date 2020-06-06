7f61eb30543a5ea7ab20a05e7a4cca6106ccf7c6
Jialin Ouyang <Jialin.Ouyang@gmail.com>
2020-06-06 00:14:23 -0700

    [Template][math] ext_gcd

--- Template/math/ext_gcd.h
+++ Template/math/ext_gcd.h
@@ -1,20 +1,24 @@
-#include <algorithm>
+#include <tuple>
 
 using namespace std;
 
 namespace math {
 
+// ax+by=gcd(a, b) and the return x, y satisfy:
+//   abs(x)+abs(y) is minimum, if multiple minimums, x <= y
 template<typename V = int32_t>
-inline V extGcd(const V& a, const V& b, V& x0, V& x1) {
-  if (!b) {
-    x0 = 1;
-    x1 = 0;
-    return a;
+inline V extGcd(V a, V b, V& x, V& y) {
+  x = 1;
+  y = 0;
+  V xTmp = 0;
+  V yTmp = 1;
+  while (b) {
+    V q = a / b;
+    tie(x, xTmp) = make_tuple(xTmp, x - q * xTmp);
+    tie(y, yTmp) = make_tuple(yTmp, y - q * yTmp);
+    tie(a, b) = make_tuple(b, a - q * b);
   }
-  V gcd = extGcd(b, a % b, x0, x1);
-  swap(x0, x1);
-  x1 -= a / b * x0;
-  return gcd;
+  return a;
 }
 
 } // namespace math
