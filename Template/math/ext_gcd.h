f53b38c86fda5f17e02264bbf58b0d02fc3beb1f
Jialin Ouyang <Jialin.Ouyang@gmail.com>
2020-06-06 16:26:30 -0700

    [Template][math] ext_gcd handles non positive numbers

--- Template/math/ext_gcd.h
+++ Template/math/ext_gcd.h
@@ -8,6 +8,16 @@ namespace math {
 //   abs(x)+abs(y) is minimum, if multiple minimums, x <= y
 template<typename V = int32_t>
 inline V extGcd(V a, V b, V& x, V& y) {
+  bool negativeA = false;
+  if (a < 0) {
+    negativeA = true;
+    a = -a;
+  }
+  bool negativeB = false;
+  if (b < 0) {
+    negativeB = true;
+    b = -b;
+  }
   x = 1;
   y = 0;
   V xTmp = 0;
@@ -17,6 +27,12 @@ inline V extGcd(V a, V b, V& x, V& y) {
     tie(x, xTmp) = make_tuple(xTmp, x - q * xTmp);
     tie(y, yTmp) = make_tuple(yTmp, y - q * yTmp);
     tie(a, b) = make_tuple(b, a - q * b);
+  }
+  if (negativeA) {
+    x = -x;
+  }
+  if (negativeB) {
+    y = -y;
   }
   return a;
 }
--- Template/math/tests/ext_gcd_test.cpp
+++ Template/math/tests/ext_gcd_test.cpp
@@ -20,4 +20,32 @@ TEST(ExtGcd, test) {
   EXPECT_EQ(y, -1);
 }
 
+TEST(ExtGcd, testNonPositive) {
+  const long long kA = 152441433273129;
+  const long long kB = 1238123131666861861;
+  const long long kGcd = 1238123123;
+
+  long long x, y;
+
+  EXPECT_EQ(extGcd<long long>(kA, kB, x, y), kGcd);
+  EXPECT_EQ(x, 71814365);
+  EXPECT_EQ(y, -8842);
+  EXPECT_EQ(kA * x + kB * y, kGcd);
+
+  EXPECT_EQ(extGcd<long long>(-kA, kB, x, y), kGcd);
+  EXPECT_EQ(x, -71814365);
+  EXPECT_EQ(y, -8842);
+  EXPECT_EQ(-kA * x + kB * y, kGcd);
+
+  EXPECT_EQ(extGcd<long long>(kA, -kB, x, y), kGcd);
+  EXPECT_EQ(x, 71814365);
+  EXPECT_EQ(y, 8842);
+  EXPECT_EQ(kA * x - kB * y, kGcd);
+
+  EXPECT_EQ(extGcd<long long>(-kA, -kB, x, y), kGcd);
+  EXPECT_EQ(x, -71814365);
+  EXPECT_EQ(y, 8842);
+  EXPECT_EQ(-kA * x - kB * y, kGcd);
+}
+
 } // namespace math
