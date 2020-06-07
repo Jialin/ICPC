d282c578b818079846ecb580f26f3dfd48603fcd
Jialin Ouyang <Jialin.Ouyang@gmail.com>
2020-06-07 15:13:41 -0700

    [Template][math][mod] fib utils

---
+++ Template/math/mod/fib_utils.h
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "math/matrix/matrix_mod_exp_utils.h"
+#include "math/mod/add.h"
+
+namespace math {
+
+template<typename V = int, typename V_SQR = long long>
+class FibUtils {
+public:
+  inline FibUtils(const V& mod = 1000000007, int capacity = -1) {
+    mod_ = mod;
+    Matrix<V> base(2, 2);
+    base.at(0, 1) = 1;
+    base.at(1, 0) = 1;
+    base.at(1, 1) = 1;
+    expUtils_.init(std::move(base), mod, capacity);
+  }
+
+  template<typename EXP = int>
+  inline V exp(EXP e) {
+    res_.init(2, 2);
+    expUtils_.template exp<EXP>(res_, e);
+    return res_.at(1, 0);
+  }
+
+private:
+  V mod_;
+  MatrixModExpUtils<V, V_SQR> expUtils_;
+  Matrix<V> res_;
+};
+
+} // namespace math
---
+++ Template/math/mod/tests/fib_utils_test.cpp
@@ -0,0 +1,40 @@
+#include "gtest/gtest.h"
+
+#include "math/mod/fib_utils.h"
+
+namespace math {
+
+TEST(FibUtils, testSmall) {
+  FibUtils<> fibUtils;
+  EXPECT_EQ(fibUtils.exp(0), 0);
+  EXPECT_EQ(fibUtils.exp(1), 1);
+  EXPECT_EQ(fibUtils.exp(2), 1);
+  EXPECT_EQ(fibUtils.exp(3), 2);
+  EXPECT_EQ(fibUtils.exp(4), 3);
+  EXPECT_EQ(fibUtils.exp(5), 5);
+  EXPECT_EQ(fibUtils.exp(6), 8);
+  EXPECT_EQ(fibUtils.exp(7), 13);
+}
+
+TEST(FibUtils, testMod1) {
+  FibUtils<> fibUtils(1);
+  EXPECT_EQ(fibUtils.exp(0), 0);
+  EXPECT_EQ(fibUtils.exp(1), 0);
+  EXPECT_EQ(fibUtils.exp(2), 0);
+}
+
+TEST(FibUtils, testBig) {
+  FibUtils<> fibUtils;
+  EXPECT_EQ(fibUtils.exp(30), 832040);
+  EXPECT_EQ(fibUtils.exp(31), 1346269);
+  EXPECT_EQ(fibUtils.exp(44), 701408733);
+  EXPECT_EQ(fibUtils.exp(45), 134903163);
+  EXPECT_EQ(fibUtils.exp(49), 778742000);
+  EXPECT_EQ(fibUtils.exp(50), 586268941);
+  EXPECT_EQ(fibUtils.exp(51), 365010934);
+  EXPECT_EQ(fibUtils.exp(60), 8745084);
+  EXPECT_EQ(fibUtils.exp(61), 730764433);
+  EXPECT_EQ(fibUtils.exp(62), 739509517);
+}
+
+} // namespace math
