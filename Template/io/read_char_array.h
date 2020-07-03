c0259c2143cde378db0190ff732d6ca3ad7962a7
Jialin Ouyang <Jialin.Ouyang@gmail.com>
2020-07-03 01:20:09 -0700

    [Template][io] revamp read_char_array

---
+++ Template/io/read_char_array.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "io/read_char.h"
+
+namespace io {
+
+inline bool readCharArray(char* s) {
+  char ch;
+  while (true) {
+    ch = readChar(false);
+    if (!ch) {
+      return false;
+    }
+    if (!isspace(ch)) {
+      break;
+    }
+    ++readPos;
+  }
+  *s++ = readChar(false);
+  while (true) {
+    ch = readChar(false);
+    if (isspace(ch)) {
+      break;
+    }
+    *s++ = ch;
+    ++readPos;
+  }
+  *s = '\0';
+  return true;
+}
+
+} // namespace io
