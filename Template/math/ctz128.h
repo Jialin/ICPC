8dd8c7ba3a76b7cefade4c2061c9a0b1f9230362
Jialin Ouyang <Jialin.Ouyang@gmail.com>
2020-07-04 18:43:03 -0700

    [Template][math] simplify ctz128

--- Template/math/ctz128.h
+++ Template/math/ctz128.h
@@ -3,8 +3,11 @@
 namespace math {
 
 inline int ctz128(__uint128_t n) {
+  if (!n) {
+    return 128;
+  }
   if (!static_cast<uint64_t>(n)) {
-    return !(n >> 64) ? 128 : __builtin_ctzll(n >> 64) + 64;
+    return __builtin_ctzll(n >> 64) + 64;
   } else {
     return __builtin_ctzll(n);
   }
