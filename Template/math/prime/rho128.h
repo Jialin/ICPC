4355e244035cf8706c6243f0eabca2ae4505ea38
Jialin Ouyang <Jialin.Ouyang@gmail.com>
2020-07-05 16:39:36 -0700

    [Template][math][prime] rho 128

---
+++ Template/math/prime/rho128.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#include <algorithm>
+
+using namespace std;
+
+#include "math/gcd128.h"
+#include "math/mod/add.h"
+#include "math/montgomery_mul.h"
+
+namespace math {
+
+namespace {
+
+const int RHO_STEP = 128;
+
+inline __uint128_t
+rhoF(const MontgomeryMul& mont, __uint128_t x, __uint128_t c) {
+  return addMod(mont.mul(x, x), c, mont.mod);
+}
+
+} // namespace
+
+inline __uint128_t rho128(
+    __uint128_t n,
+    MontgomeryMul& mont,
+    bool initMont = true,
+    __uint128_t x = 0,
+    __uint128_t c = 0) {
+  if (!(n & 1)) {
+    return 2;
+  }
+  if (initMont) {
+    mont.init(n);
+  }
+  if (!x) {
+    x = mont.convert(rand());
+  }
+  if (!c) {
+    c = mont.convert(rand());
+  }
+  __uint128_t g = 1, q = 1, xs = 0, y = 0;
+  for (int l = 1; g == 1; l <<= 1) {
+    y = x;
+    for (int i = 1; i < l; ++i) {
+      x = rhoF(mont, x, c);
+    }
+    for (int k = 0; k < l && g == 1; k += RHO_STEP) {
+      xs = x;
+      for (int i = min(l - k, RHO_STEP); i > 0; --i) {
+        x = rhoF(mont, x, c);
+        q = mont.mul(q, y > x ? y - x : x - y);
+      }
+      g = gcd128(q, n);
+    }
+  }
+  if (g == n) {
+    do {
+      xs = rhoF(mont, xs, c);
+      g = gcd128(xs > y ? xs - y : y - xs, n);
+    } while (g == 1);
+  }
+  return g;
+}
+
+} // namespace math
---
+++ Template/math/prime/tests/rho128_test.cpp
@@ -0,0 +1,50 @@
+#include "gtest/gtest.h"
+
+#include "math/prime/rho128.h"
+
+namespace math {
+
+TEST(Rho128, testSmall) {
+  MontgomeryMul mont;
+  EXPECT_EQ(rho128(4, mont, true, 1, 2), 2);
+  EXPECT_EQ(rho128(9, mont, true, 1, 2), 3);
+  EXPECT_EQ(rho128(121, mont, true, 1, 2), 11);
+}
+
+TEST(Rho128, testLarge) {
+  MontgomeryMul mont;
+
+  __uint128_t v = rho128(3000000014ULL, mont, true, 1, 2);
+  EXPECT_TRUE(v == 2 || v == 1000000007);
+
+  EXPECT_EQ(rho128(1000000014000000049ULL, mont, true, 1, 2), 1000000007);
+
+  EXPECT_EQ(rho128(1000000014000000049ULL, mont, true, 1, 2), 1000000007);
+
+  v = rho128(
+      __uint128_t(9616562014629204ULL) * 100000000000000ULL + 97194293996611ULL,
+      mont,
+      true,
+      1,
+      2);
+  EXPECT_TRUE(
+      v == 973825889 || v == __uint128_t(98750322036562790) * 10000 + 2499);
+
+  v = rho128(
+      __uint128_t(9553427693635611ULL) * 100000000000000ULL + 1863533963531ULL,
+      mont,
+      true,
+      1,
+      2);
+  EXPECT_TRUE(v == 973806882626147ULL || v == 981039245468473ULL);
+
+  v = rho128(
+      __uint128_t(9637823792045106ULL) * 100000000000000ULL + 91122291047909ULL,
+      mont,
+      true,
+      1,
+      2);
+  EXPECT_TRUE(v == 244564652505673ULL || v == 3940808163935933ULL);
+}
+
+} // namespace math
