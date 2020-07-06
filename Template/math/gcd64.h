cb3327f5ed46cf00050f9697150b0abd5d50dd8f
Jialin Ouyang <Jialin.Ouyang@gmail.com>
2020-07-05 23:47:25 -0700

    [Template][math] gcd 64 remove debug code

--- Template/math/gcd64.h
+++ Template/math/gcd64.h
@@ -11,19 +11,14 @@ inline uint64_t gcd64(uint64_t a, uint64_t b) {
     return a | b;
   }
   auto shift = __builtin_ctzll(a | b);
-  printf("shift:%d\n", shift);
   a >>= __builtin_ctzll(a);
-  printf("\ta:%llu\n", a);
   do {
-    printf("\ta:%llu b:%llu\n", a, b);
     b >>= __builtin_ctzll(b);
     if (a > b) {
       swap(a, b);
     }
     b -= a;
-    // printf("\ta:%llu b:%llu\n", a, b);
   } while (b);
-  printf("\ta:%llu\n", a);
   return a << shift;
 }
 
