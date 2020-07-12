496f6d3cc55628e09eadaf4111ef3fff65778095
Jialin Ouyang <Jialin.Ouyang@gmail.com>
2020-07-12 13:51:08 -0700

    [Tempate][math] gcd32

---
+++ Template/math/gcd32.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <algorithm>
+
+using namespace std;
+
+namespace math {
+
+inline uint32_t gcd32(uint32_t a, uint32_t b) {
+  if (!a || !b) {
+    return a | b;
+  }
+  int shift = __builtin_ctz(a | b);
+  a >>= __builtin_ctz(a);
+  do {
+    b >>= __builtin_ctz(b);
+    if (a > b) {
+      swap(a, b);
+    }
+    b -= a;
+  } while (b);
+  return a << shift;
+}
+
+} // namespace math
