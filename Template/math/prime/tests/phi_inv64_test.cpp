c3213c52fe080ecb33c6ea48f5e8d53366eb8963
Jialin Ouyang <Jialin.Ouyang@gmail.com>
2020-07-26 18:47:38 -0700

    [Template][math][prime] enhance phi_inv 64 test

--- Template/math/prime/tests/phi_inv64_test.cpp
+++ Template/math/prime/tests/phi_inv64_test.cpp
@@ -36,6 +36,48 @@ TEST(PhiInv64Test, basic) {
   EXPECT_EQ(vs[2], 20);
   EXPECT_EQ(vs[3], 24);
   EXPECT_EQ(vs[4], 30);
+
+  vs.clear();
+  phiInv.calc(10, processor);
+  ASSERT_EQ(vs.size(), 2);
+  sort(vs.begin(), vs.end());
+  EXPECT_EQ(vs[0], 11);
+  EXPECT_EQ(vs[1], 22);
+
+  vs.clear();
+  phiInv.calc(20, processor);
+  ASSERT_EQ(vs.size(), 5);
+  sort(vs.begin(), vs.end());
+  EXPECT_EQ(vs[0], 25);
+  EXPECT_EQ(vs[1], 33);
+  EXPECT_EQ(vs[2], 44);
+  EXPECT_EQ(vs[3], 50);
+  EXPECT_EQ(vs[4], 66);
+
+  vs.clear();
+  phiInv.calc(30, processor);
+  ASSERT_EQ(vs.size(), 2);
+  sort(vs.begin(), vs.end());
+  EXPECT_EQ(vs[0], 31);
+  EXPECT_EQ(vs[1], 62);
+
+  vs.clear();
+  phiInv.calc(40, processor);
+  ASSERT_EQ(vs.size(), 9);
+  sort(vs.begin(), vs.end());
+  EXPECT_EQ(vs[0], 41);
+  EXPECT_EQ(vs[1], 55);
+  EXPECT_EQ(vs[2], 75);
+  EXPECT_EQ(vs[3], 82);
+  EXPECT_EQ(vs[4], 88);
+  EXPECT_EQ(vs[5], 100);
+  EXPECT_EQ(vs[6], 110);
+  EXPECT_EQ(vs[7], 132);
+  EXPECT_EQ(vs[8], 150);
+
+  vs.clear();
+  phiInv.calc(50, processor);
+  ASSERT_TRUE(vs.empty());
 }
 
 } // namespace math
