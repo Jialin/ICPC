3b288f78c31d8e39bbb67e5f734e406ecf15749b
Jialin Ouyang <Jialin.Ouyang@gmail.com>
2020-07-26 18:47:25 -0700

    [Template][math][prime] divisor iterator 32

---
+++ Template/math/prime/divisor32.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <functional>
+#include <vector>
+
+using namespace std;
+
+namespace math {
+
+namespace {
+
+inline bool divisorIterator32Dfs_(
+    int idx,
+    const vector<int>& primes,
+    uint32_t x,
+    uint32_t remN,
+    uint32_t n,
+    const std::function<bool(uint32_t)>& processor) {
+  if (remN == 1) {
+    auto y = n / x;
+    if (!processor(x)) {
+      return false;
+    }
+    if (x < y) {
+      if (!processor(y)) {
+        return false;
+      }
+    }
+    return true;
+  }
+  int size = static_cast<int>(primes.size());
+  uint32_t p = 0, nxtRemN;
+  int cnt = 0;
+  for (; idx < size && static_cast<uint32_t>(primes[idx]) <= remN / primes[idx];
+       ++idx) {
+    p = primes[idx];
+    if (!(remN % p)) {
+      cnt = 1;
+      break;
+    }
+  }
+  if (!cnt) {
+    p = remN;
+    cnt = 1;
+    nxtRemN = 1;
+  } else {
+    for (nxtRemN = remN / p; !(nxtRemN % p); ++cnt, nxtRemN /= p) {}
+  }
+  if (!divisorIterator32Dfs_(idx + 1, primes, x, nxtRemN, n, processor)) {
+    return false;
+  }
+  for (int i = cnt; i > 0; --i) {
+    x *= p;
+    if (x > n / x) {
+      break;
+    }
+    if (!divisorIterator32Dfs_(idx + 1, primes, x, nxtRemN, n, processor)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+} // namespace
+
+inline void divisorIterator32(
+    uint32_t n,
+    const vector<int>& primes,
+    const std::function<bool(uint32_t)>& processor) {
+  divisorIterator32Dfs_(0, primes, 1, n, n, processor);
+}
+
+} // namespace math
