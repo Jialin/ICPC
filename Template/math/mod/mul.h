a383d13c0a6b8af6dbde92cbf01818e398bd1df7
Jialin Ouyang <Jialin.Ouyang@gmail.com>
2020-08-15 19:19:52 -0700

    [Template][math][mod] introduce mul which is more efficient than mul_safe

---
+++ Template/math/mod/mul.h
@@ -0,0 +1,14 @@
+
+#pragma once
+
+#include "math/mod/mul_inline.h"
+
+namespace math {
+
+template<typename V = int32_t, typename V_SQR = int64_t>
+inline V mulMod(V a, const V& b, const V& mod) {
+  mulModInline<V, V_SQR>(a, b, mod);
+  return a;
+}
+
+} // namespace math
