bc797bbea26cd059b9ed01d8e0d7f84593c69d68
Jialin Ouyang <Jialin.Ouyang@gmail.com>
2020-08-16 14:59:18 -0700

    [setup] generate debug file which contains debug info

--- setup/cpp_gen.py
+++ setup/cpp_gen.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import sys
+import re
 
 INCLUDES = [
     "algorithm",
@@ -23,10 +24,61 @@ INCLUDES = [
     "utility",
     "vector",
 ]
+DUMMY_LINE_PATTERN = re.compile(r"^\s*;$")
 
 
 def default_includes():
     return ["#include <{}>".format(include) for include in INCLUDES]
+
+
+def gen_file(prefix, info, additional_args=None):
+    proc = subprocess.Popen(
+        [
+            "g++",
+            file_name,
+            "-O2",
+            "-std=c++14",
+            "-I",
+            os.path.join(os.environ["ICPC_HOME"], "Template"),
+            "-E",
+            "-nostdinc",
+            "-nostdinc++",
+            "-C",
+        ]
+        + (additional_args or []),
+        stdout=subprocess.PIPE,
+    )
+    proc.wait()
+    clean_lines = []
+    using_std = False
+    for line in proc.stdout.read().split(b"\n"):
+        if not line.startswith(b"#"):
+            utf8_line = line.decode("utf-8")
+            if utf8_line == "using namespace std;":
+                if using_std:
+                    continue
+                using_std = True
+            if DUMMY_LINE_PATTERN.match(utf8_line):
+                continue
+            clean_lines.append(utf8_line)
+    gen_file_name = prefix + file_name
+    file = open(gen_file_name, "w")
+    file.write("\n".join(default_includes() + clean_lines))
+    file.close()
+    subprocess.Popen(["clang-format", "-i", gen_file_name]).wait()
+    with open(gen_file_name, "r") as input_file:
+        subprocess.Popen(["pbcopy"], stdin=input_file).wait()
+    print(
+        """\033[92m
+{separator}
+! {gen_file_name} {info} !
+{separator}
+\033[0m""".format(
+            gen_file_name=gen_file_name,
+            separator="!" * (len(gen_file_name) + len(info) + 5),
+            info=info,
+        )
+    )
 
 
 if len(sys.argv) < 2:
@@ -39,45 +91,5 @@ if not file_name.endswith(".cpp"):
 if not os.path.exists(file_name):
     print("{} not found.".format(file_name))
     sys.exit(1)
-proc = subprocess.Popen(
-    [
-        "g++",
-        file_name,
-        "-O2",
-        "-std=c++14",
-        "-I",
-        os.path.join(os.environ["ICPC_HOME"], "Template"),
-        "-E",
-        "-nostdinc",
-        "-nostdinc++",
-        "-C",
-    ],
-    stdout=subprocess.PIPE,
-)
-proc.wait()
-clean_lines = []
-using_std = False
-for line in proc.stdout.read().split(b"\n"):
-    if not line.startswith(b"#"):
-        utf8_line = line.decode("utf-8")
-        if utf8_line == "using namespace std;":
-            if using_std:
-                continue
-            using_std = True
-        clean_lines.append(utf8_line)
-gen_file_name = "gencpp_" + file_name
-file = open(gen_file_name, "w")
-file.write("\n".join(default_includes() + clean_lines))
-file.close()
-subprocess.Popen(["clang-format", "-i", gen_file_name]).wait()
-with open(gen_file_name, "r") as input_file:
-    subprocess.Popen(["pbcopy"], stdin=input_file).wait()
-print(
-    """
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-! {} is generated and copied to clipboard
-!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-""".format(
-        gen_file_name
-    )
-)
+gen_file(prefix="gendebug-", info="is generated", additional_args=["-DLOCAL"])
+gen_file(prefix="gencpp-", info="is generated and copied to clipboard")
