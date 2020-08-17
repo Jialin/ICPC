978f1e8cd1e5d7226d9021c1b4af421cfe6fe30f
Jialin Ouyang <Jialin.Ouyang@gmail.com>
2020-08-17 00:33:01 -0700

    [Template][debug] DEBUG_EQ

--- Template/debug/debug.h
+++ Template/debug/debug.h
@@ -3,14 +3,18 @@
 #ifndef LOCAL
 
 #define DEBUG(args...)
+#define DEBUG_EQ(x, y)
 
 #else
 
-#define DEBUG_BEGIN fprintf(stderr, "\033[94m");
+#define DEBUG_BEGIN fprintf(stderr, "\033[94m[%d] ", __LINE__);
 #define DEBUG_END fprintf(stderr, "\033[0m");
 #define DEBUG(args...)                                                         \
   DEBUG_BEGIN;                                                                 \
   fprintf(stderr, args);                                                       \
   DEBUG_END
-
+#define DEBUG_EQ(x, y)                                                         \
+  if (x != y) {                                                                \
+    DEBUG("%d != %d\n", x, y);                                                 \
+  }
 #endif
