59be1a566db6fc3e32d3d0977067f3d646ab37da
Jialin Ouyang <Jialin.Ouyang@gmail.com>
2020-08-23 02:10:26 -0700

    [Template][debug] enhance DEBUG format

--- Template/debug/debug.h
+++ Template/debug/debug.h
@@ -2,17 +2,19 @@
 
 #ifndef LOCAL
 
-#define DEBUG(args...)
+#define DEBUG(fmt, args...)
 #define DEBUG_EQ(x, y)
 
 #else
 
-#define DEBUG_BEGIN fprintf(stderr, "\033[94m[%d] ", __LINE__);
-#define DEBUG_END fprintf(stderr, "\033[0m");
-#define DEBUG(args...)                                                         \
-  DEBUG_BEGIN;                                                                 \
-  fprintf(stderr, args);                                                       \
-  DEBUG_END
+#define DEBUG(fmt, args...)                                                    \
+  fprintf(                                                                     \
+      stderr,                                                                  \
+      ("\033[94m[%s][L%d] " + string(fmt) + "\033[0m").c_str(),                \
+      __PRETTY_FUNCTION__,                                                     \
+      __LINE__,                                                                \
+      args)
+
 #define DEBUG_EQ(x, y)                                                         \
   if (x != y) {                                                                \
     DEBUG("%d != %d\n", x, y);                                                 \
