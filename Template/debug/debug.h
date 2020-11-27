44370cd9a0f19898f7b7216d3a78077871721175
Jialin <Jialin.Ouyang@gmail.com>
2020-11-27 14:39:53 -0800

    [template][debug] DEBUGV

--- Template/debug/debug.h
+++ Template/debug/debug.h
@@ -3,6 +3,7 @@
 #ifndef LOCAL
 
 #define DEBUG(fmt, args...)
+#define DEBUGV(v)
 #define DEBUG_EQ(x, y)
 #define DEBUG_FALSE(statement, fmt, args...)
 #define DEBUG_TRUE(statement, fmt, args...)
@@ -11,6 +12,37 @@
 
 #include <boost/stacktrace.hpp>
 
+#define DEBUG_BEGIN(function, line)                                            \
+  fprintf(stderr, "\033[94m[%s][L%d]\n\t", prettyFunction.c_str(), line)
+#define DEBUG_END fprintf(stderr, "\n\033[0m")
+
+void debugv(
+    const vector<int>& vs,
+    const string& name,
+    const string& prettyFunction,
+    int line) {
+  DEBUG_BEGIN(prettyFunction, line);
+  fprintf(stderr, "vector<int>`%s`(size:%lu)= [", name.c_str(), vs.size());
+  if (!vs.empty()) {
+    fprintf(stderr, "%d", vs.front());
+    for (int i = 1; i < (int)vs.size(); ++i) {
+      fprintf(stderr, ",%d", vs[i]);
+    }
+  }
+  fprintf(stderr, "]");
+  DEBUG_END;
+}
+
+void debugv(
+    const string& v,
+    const string& name,
+    const string& prettyFunction,
+    int line) {
+  DEBUG_BEGIN(prettyFunction, line);
+  fprintf(stderr, "string`%s`= %s", name.c_str(), v.c_str());
+  DEBUG_END;
+}
+
 #define DEBUG_STACKTRACE(fmt, args...)                                         \
   fprintf(                                                                     \
       stderr,                                                                  \
@@ -18,7 +50,9 @@
       __PRETTY_FUNCTION__,                                                     \
       __LINE__,                                                                \
       args,                                                                    \
-      boost::stacktrace::to_string(boost::stacktrace::stacktrace()).c_str());
+      boost::stacktrace::to_string(boost::stacktrace::stacktrace()).c_str())
+
+#define DEBUGV(v) debugv(v, #v, __PRETTY_FUNCTION__, __LINE__)
 
 #define DEBUG(fmt, args...)                                                    \
   fprintf(                                                                     \
@@ -26,7 +60,7 @@
       ("\033[94m[%s][L%d] " + string(fmt) + "\033[0m").c_str(),                \
       __PRETTY_FUNCTION__,                                                     \
       __LINE__,                                                                \
-      args);
+      args)
 
 #define DEBUG_EQ(x, y)                                                         \
   if (x != y) {                                                                \
