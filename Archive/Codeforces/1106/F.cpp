aab0a6d7c087f4bd9f347da9efd770e7e3ffc838
Jialin <Jialin.Ouyang@gmail.com>
2020-12-05 22:46:07 -0800

    [Archive] kitamasa #2

--- Archive/Codeforces/1106/F.cpp
+++ Archive/Codeforces/1106/F.cpp
@@ -21,34 +21,34 @@
 using namespace std;
 
 #include "debug/debug.h"
-#include "math/matrix/kitamasa.h"
+#include "math/kitamasa/kitamasa_once.h"
 #include "math/mod/root.h"
 
 const int MOD = 998244353;
 const int MOD_ROOT = 3;
 
+math::KitamasaOnce<> ki;
 math::LogModCoPrime<> logMod;
-math::Kitamasa<> ki;
 
 int main() {
   int n;
-  scanf("%d", &n);
-  vector<int> coef(n);
-  for (int i = 0; i < n; ++i) {
-    scanf("%d", &coef[n - 1 - i]);
+  while (scanf("%d", &n) != EOF) {
+    vector<int> coefs(n);
+    for (int i = n - 1; i >= 0; --i) {
+      scanf("%d", &coefs[i]);
+    }
+    int exp, v;
+    scanf("%d%d", &exp, &v);
+    if (!v) {
+      puts("-1");
+      return 0;
+    }
+    vector<int> xs(n);
+    xs[n - 1] = 1;
+    int res = ki.calc(coefs, xs, exp - 1, MOD - 1);
+    bool exist;
+    logMod.init(100003, false, 100000);
+    int resBase = math::rootMod(res, v, MOD, MOD_ROOT, logMod, exist);
+    printf("%d\n", exist ? resBase : -1);
   }
-  ki.init(coef, MOD - 1, 32);
-  int exp, v;
-  scanf("%d%d", &exp, &v);
-  if (!v) {
-    puts("-1");
-    return 0;
-  }
-  vector<int> vs(n);
-  vs[n - 1] = 1;
-  int res = ki.calc(vs, exp - 1);
-  bool exist;
-  logMod.init(100003, false, 100000);
-  int resBase = math::rootMod(res, v, MOD, MOD_ROOT, logMod, exist);
-  printf("%d\n", exist ? resBase : -1);
 }
