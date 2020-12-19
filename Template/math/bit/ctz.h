dd112a94fd03db8e5d1f332b96dd6f25c10340e7
Jialin <Jialin.Ouyang@gmail.com>
2020-12-19 12:27:33 -0800

    [Template][math][bit] ctz clang diagnostic suppress

--- Template/math/bit/ctz.h
+++ Template/math/bit/ctz.h
@@ -11,7 +11,10 @@ inline int ctz(T n) {
   if (!n) {
     return numeric_limits<T>::digits + numeric_limits<T>::is_signed;
   }
-  for (int res = 0; n; res += 32, n >>= 16, n >>= 16) {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wshift-count-overflow"
+  for (int res = 0; n; res += 32, n >>= 32) {
+#pragma clang diagnostic pop
     uint32_t part = static_cast<uint32_t>(n & 0xFFFFFFFFU);
     if (part) {
       return res + __builtin_ctz(part);
