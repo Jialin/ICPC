7e34e46f8b371b5747e497b16294ce025de6cb7c
Jialin <Jialin.Ouyang@gmail.com>
2021-11-07 22:56:14 -0800

    [next-pow2] simplify

--- Template/math/bit/next_pow2_32.h
+++ Template/math/bit/next_pow2_32.h
@@ -6,7 +6,7 @@ inline uint32_t nextPow2_32(uint32_t v) {
   if (!v) {
     return 1;
   }
-  uint32_t res = 1U << (31 - __builtin_clz(v));
+  uint32_t res = 1U << (31 ^ __builtin_clz(v));
   return res == v ? res : res << 1;
 }
 
