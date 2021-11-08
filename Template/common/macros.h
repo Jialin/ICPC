c1489ab43c431f37ed54558e20ce54450742d860
Jialin <Jialin.Ouyang@gmail.com>
2021-11-07 22:53:56 -0800

    [macros] fix

--- Template/common/macros.h
+++ Template/common/macros.h
@@ -2,12 +2,12 @@
 #define CAST static_cast
 #define FILL(vs, v) fill(ALL(vs), v)
 #define FOR(i, a, b) for (int i = (a); i < (b); ++i)
-#define FOREDGE(i, j, vs) for (int i = vs.lastOut[j]; i >= 0; i = vs.nxtOut[i])
+#define FOREDGE(i, j, vs) for (int i = (vs).lastOut[j]; i >= 0; i = (vs).nxtOut[i])
 #define FORIF(i, a, b, condition)                                                                  \
   for (int i = (a); i < (b); ++i)                                                                  \
     if (condition)
-#define FORSIZE(i, vs) for (int i = 0; i < static_cast<int>(vs.size()); ++i)
-#define SIZE(vs) static_cast<int>(vs.size())
+#define SIZE(vs) static_cast<int>((vs).size())
+#define FORSIZE(i, vs) for (int i = 0; i < SIZE(vs); ++i)
 #define SORT(vs) sort(ALL(vs))
 #define UNIQUE(vs) vs.erase(unique(ALL(vs)), vs.end())
 #define SORTUNIQUE(vs)                                                                             \
