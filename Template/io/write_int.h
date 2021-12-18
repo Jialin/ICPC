c4fb358804296b59d5af8d0b9a62cdbd171d3d46
Jialin <Jialin.Ouyang@gmail.com>
2021-12-18 13:42:15 +0800

    Update write_int.h

--- Template/io/write_int.h
+++ Template/io/write_int.h
@@ -5,7 +5,7 @@
 namespace io {
 
 template<class T>
-inline void writeInt(T x, int padding = 0) {
+inline void writeInt(T x, char c = 0, int padding = 0) {
   static char s[32];
   if (x < 0) {
     writeChar('-');
@@ -14,13 +14,16 @@ inline void writeInt(T x, int padding = 0) {
   int n = 0;
   while (x || !n) {
     T newX = x / 10;
-    s[n++] = '0' + (x - newX * 10);
+    s[n++] = '0' + (x - (newX << 3) - (newX << 1));
     x = newX;
   }
   for (int i = n; i < padding; ++i) {
     writeChar('0');
   }
   for (; n > 0; writeChar(s[--n])) {}
+  if (c) {
+    writeChar(c);
+  }
 }
 
 } // namespace io
