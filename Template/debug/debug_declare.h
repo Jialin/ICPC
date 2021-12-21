340afc4aecb449c45a244fdcaaa920ea9af91140
Jialin <Jialin.Ouyang@gmail.com>
2021-12-21 13:51:32 +0800

    [template] debug_declare

--- Template/debug/debug_declare.h
+++ Template/debug/debug_declare.h
@@ -3,15 +3,16 @@
 #ifdef LOCAL
 using namespace std;
 
+inline string totype(int v);
+inline string tostring(int v);
+
 template<typename T>
 inline string totype(const vector<T>& vs);
-
-template<typename X, typename Y>
-inline string totype(const pair<X, Y>& vs);
-
 template<typename T>
 inline string tostring(const vector<T>& vs);
 
+template<typename X, typename Y>
+inline string totype(const pair<X, Y>& vs);
 template<typename X, typename Y>
 inline string tostring(const pair<X, Y>& vs);
 
