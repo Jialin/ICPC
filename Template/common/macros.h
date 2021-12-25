8c3816b591cb5166313c806dd436f41c7f180be3
Jialin <Jialin.Ouyang@gmail.com>
2021-12-25 21:45:25 +0800

    Update macros.h

--- Template/common/macros.h
+++ Template/common/macros.h
@@ -1,12 +1,17 @@
 #define ALL(vs) vs.begin(), vs.end()
 #define CAST static_cast
 #define FILL(vs, v) fill(ALL(vs), v)
+
 #define FOR(i, a, b) for (int i = (a); i < (b); ++i)
 #define FORR(i, a, b) for (int i = (a); i >= (b); --i)
-#define FOREDGE(i, j, vs) for (int i = (vs).lastOut[j]; i >= 0; i = (vs).nxtOut[i])
 #define FORIF(i, a, b, condition)                                                                  \
   for (int i = (a); i < (b); ++i)                                                                  \
     if (condition)
+#define FORRIF(i, a, b, condition)                                                                 \
+  for (int i = (a); i >= (b); --i)                                                                 \
+    if (condition)
+
+#define FOREDGE(i, j, vs) for (int i = (vs).lastOut[j]; i >= 0; i = (vs).nxtOut[i])
 #define SIZE(vs) static_cast<int>((vs).size())
 #define FORSIZE(i, vs) for (int i = 0; i < SIZE(vs); ++i)
 #define SORT(vs) sort(ALL(vs))
